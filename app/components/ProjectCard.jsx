a734bd4cf26fc5fbe37717b0f67a127bd5b0fc47
Dylan Peltier <peltdyla@gmail.com>
2024-07-12 02:04:30 -0400

    fixed styling issues for different dimensions

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -1,9 +1,11 @@
-import React from "react";
-import { Card, CardHeader, CardBody, Image, Button } from "@nextui-org/react";
+'use client'
 
-export function ProjectCard({ projectName, projectDescription, projectDate, projectImage }) {
+import React from "react";
+import { Card, CardHeader, Button, Image } from "@nextui-org/react";
+
+export function ProjectCard({ projectName, projectDescription, projectDate, projectImage, projectId }) {
   return (
-    <Card className="p-4 w-fit flex flex-col items-center justify-center" isPressable>
+    <Card className="p-4 w-fit max-w-[450px] h-full max-h-[300px] flex flex-col items-center justify-center">
       <CardHeader className="flex flex-col items-start">
         <h4 className="font-semibold text-large text-left pb-1">{projectName}</h4>
         <p className="w-full text-default-500 text-left text-sm pb-4">{projectDescription}</p>
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -22,7 +22,7 @@ export default async function Projects() {
     <section className="flex flex-col w-screen h-[calc(100vh-69x)] items-center justify-start py-20 gap-20">
       <div className="w-4/5 h-auto flex flex-col items-left justify-center gap-8 p-4">
       <h1 className="w-auto text-left text-4xl font-semibold text-slate-950 pb-3">Our Projects</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
           {projects.map((project, index) => (
             <div key={project.projectId}>
               <ProjectCard
