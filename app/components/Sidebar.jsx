7668aa787f23cbc6a48ff629bec028338732e5c5
Dylan Peltier <peltdyla@gmail.com>
2024-07-25 12:55:58 -0400

    Revert "fixed bug where navbar doesnt display when rerouting back to home"
    This reverts commit 62955687df1345facea20a3a0bf55fc8fbb88f3f.

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -21,19 +21,10 @@ export default function Sidebar({ toggleSidebar }) {
     }
   }, [pathname]);
 
-  const handleGoBackHome = () => {
-    // Show the navbar before redirecting
-    const navbar = document.getElementById("navbar");
-    if (navbar) {
-      navbar.style.display = "block";
-    }
-    router.push("/");
-  };
-
   return (
     <div className="flex w-fit h-screen flex-col items-start justify-start border-r-1 border-slate-300 gap-3 p-6 bg-slate-50">
       <div id="company-card" className="flex flex-row w-fit h-fit gap-3 items-center justify-center mb-8">
-        <div onClick={handleGoBackHome} className="border-1 border-slate-300 rounded-md w-fit h-fit cursor-pointer">
+        <div className="border-1 border-slate-300 rounded-md w-fit h-fit">
           <AcmeLogo />
         </div>
         <div className="flex flex-col items-start justify-center w-full h-fit">
