aa8a2f93638c553ae25bfdfbe700c05657a0e2f0
Dylan Peltier <peltdyla@gmail.com>
2024-07-24 21:05:14 -0400

    added ProjectTable component

--- app/admin/projects/page.jsx
+++ app/admin/projects/page.jsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { useEffect } from "react";
+import ProjectTable from "../../components/ProjectTable";
+import { Input, Button } from "@nextui-org/react";
 
 export default function AdminProjects() {
   useEffect(() => {
@@ -14,9 +16,13 @@ export default function AdminProjects() {
   return (
     <div className="w-full h-full flex flex-col items-start justify-start gap-10 p-10">
       <div className="w-full h-fit flex flex-col items-start justify-start gap-3">
-        <p className="text-xl font-semibold">All Projects</p>
+        <p className="text-2xl font-semibold mb-2">All Projects</p>
+        <div className="flex flex-row items-start justify-between w-full">
+          <Input placeholder="Search projects" className="w-1/6"/>
+          <Button color="primary" className="font-semibold">Add Project</Button>
+        </div>
         <div className="flex flex-row w-full h-fit gap-8 items-start justify-start">
-          Table goes here
+          <ProjectTable />
         </div>
       </div>
     </div>
