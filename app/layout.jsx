8db719e693f885d698f8bf92bab25cd4a49acdb2
Dylan Peltier <peltdyla@gmail.com>
2024-07-29 14:37:33 -0400

    viewport shenanigans

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,18 +3,19 @@ import "./globals.css";
 import Provider from "./providers";
 import Navbar from "./components/Navbar";
 import { ClerkProvider, ClerkLoading, ClerkLoaded } from "@clerk/nextjs";
-import {Spinner} from "@nextui-org/react";
-
+import { Spinner } from "@nextui-org/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Precision Sand Blasting",
   description: "Official website of Precision Sand Blasting",
+  name: "viewport",
+  content:
+    "width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover",
 };
 
 export default function RootLayout({ children }) {
-
   return (
     <ClerkProvider>
       <html lang="en">
